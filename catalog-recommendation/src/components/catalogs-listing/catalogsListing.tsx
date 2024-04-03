22d0927aeb3ba7d6f4ed6e433e1bade951361cd1
Felipe Lemos <>
2024-04-03 17:26:27 +0200

    Feat: Delete Benoit's lists

--- catalog-recommendation/src/components/catalogs-listing/catalogsListing.tsx
+++ catalog-recommendation/src/components/catalogs-listing/catalogsListing.tsx
@@ -33,6 +33,7 @@ const CatalogsListing: React.FC = () => {
   }, []);
 
   const [buLists, setBuLists] = useState<any>();
+  const [benoitsShoppingLists, setBenoitsShoppingLists] = useState<any[]>([]);
 
   useMemo(() => {
     setBuLists(
@@ -42,11 +43,22 @@ const CatalogsListing: React.FC = () => {
     );
   }, [buSelection]);
 
+  useMemo(() => {
+    setBenoitsShoppingLists(
+      shoppingLists.filter(
+        (shoppingList) =>
+          shoppingList.customer.id === '6f05bc8c-b93b-4178-bd0d-7b0d680a1faa'
+      )
+    );
+  }, [shoppingLists]);
+
   return (
     <>
       <PageContentNarrow>
         <div className="pb-5">
-          <Text.Headline as="h1">Catalog Recommendation Uploader</Text.Headline>
+          <Text.Headline as="h1">
+            Catalog Recommendations Management
+          </Text.Headline>
         </div>
         <BusinessUnitSelector
           setSelection={setBuSelection}
@@ -95,6 +107,44 @@ const CatalogsListing: React.FC = () => {
             setBuSelection('');
           }}
         />
+        <div>
+          <hr className="mt-10 mb-5" />
+          <Text.Headline as="h2">
+            <p className="text-red-600">Danger zone</p>
+          </Text.Headline>
+          <PrimaryButton
+            style={{ width: 280, textAlign: 'justify', marginTop: 10 }}
+            label="Delete All Catalog Recommendations"
+            type="button"
+            size="big"
+            isDisabled={benoitsShoppingLists?.length < 1}
+            onClick={() => {
+              benoitsShoppingLists.forEach((shoppingList: any) => {
+                deleteShoppingListById(
+                  shoppingList.id,
+                  shoppingList.version
+                ).then((response: any) => {
+                  if (response.statusCode) {
+                    showNotification({
+                      kind: NOTIFICATION_KINDS_PAGE.error,
+                      domain: DOMAINS.PAGE,
+                      text: response.message,
+                    });
+                  } else {
+                    showNotification({
+                      kind: NOTIFICATION_KINDS_SIDE.success,
+                      domain: DOMAINS.SIDE,
+                      text: `Recommendation List ${shoppingList.name['fr-FR']} deleted!`,
+                    });
+                  }
+                });
+              });
+              setBenoitsShoppingLists([]);
+              setBuLists([]);
+              setBuSelection('');
+            }}
+          />
+        </div>
       </PageContentNarrow>
     </>
   );
